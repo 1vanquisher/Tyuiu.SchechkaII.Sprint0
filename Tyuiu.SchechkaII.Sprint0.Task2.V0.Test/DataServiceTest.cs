b04bb71bff053793811f1d9595826e1a5bb41ad8
ivan.shechka38 <ivan.shechka38@gmail.com>
2026-03-25 12:42:46 +0500

    Task3

--- Tyuiu.SchechkaII.Sprint0.Task2.V0.Test/DataServiceTest.cs
+++ Tyuiu.SchechkaII.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -10,11 +10,11 @@ namespace Tyuiu.SchechkaII.Sprint0.Task2.V0.Test
         [TestMethod]
         public void CheckGetMessageValid()
         {
-            // Область создания методов тестирования, методов из библиотеки
+           
             var name = "Иван";
             var res = DataService.GetMessage(name);
 
-            //Вызываем класс Assert и метод AreEqual
+            
             Assert.AreEqual("Привет..., Иван", res);
         }
     }
---
+++ Tyuiu.SchechkaII.Sprint0.Task3.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SchechkaII.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SchechkaII.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ddfeeccb-c44e-4226-9085-f48b72efa0ed")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
